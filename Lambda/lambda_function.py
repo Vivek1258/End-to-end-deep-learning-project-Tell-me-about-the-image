374c268da3a82850517b95bc5a6265018e17d860
Vivek Mankar <vivek.mankar18@vit.edu>
2021-03-13 16:28:29 +0530

    Update lambda_function.py

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -6,16 +6,16 @@ from SME_caller import gen_caption
 
 def lambda_handler(event, context):
     # TODO implement
-    
+     
     data = json.loads(event['body'])
     
     data = json.loads(data) 
     
     data = data['data']
     
-    img_features = np.asarray(data)  
+    img = np.asarray(data)  
     
-    caption = gen_caption(img_features)
+    caption = gen_caption(img)
     
     return {
         'statusCode': 200,
