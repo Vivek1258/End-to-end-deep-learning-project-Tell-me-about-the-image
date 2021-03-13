899db5fc9a11efd55f49b8e28f32913bfc9fef33
Vivek Mankar <vivek.mankar18@vit.edu>
2021-03-13 16:28:58 +0530

    Update SME_caller.py

--- Lambda/SME_caller.py
+++ Lambda/SME_caller.py
@@ -1,19 +1,23 @@
+
 import boto3
 import json
 import numpy as np 
 
 # define gloabel vareables
-ENDPOINT_NAME = 'tensorflow-inference-2021-03-01-16-42-22-041'
+ENDPOINT_NAME = 'tensorflow-inference-2021-03-12-16-23-57-315'
 sm_runtime_client= boto3.client('runtime.sagemaker')
 
+## Note : Change the timeout settings to 10 sec
 
-def gen_caption(img_features):
+def gen_caption(img):
     
     """ Function to generate caption for the given image """
     
     """ Sagemaker Model End Point caller function """
     
-    image_list = img_features.tolist()
+    print("the shape of the imge is " , img.shape)
+    
+    image_list = img.tolist()
     image_json = json.dumps(image_list)
     
     ## invoke endpoint for inference
@@ -25,4 +29,3 @@ def gen_caption(img_features):
     caption = json.loads(response['Body'].read().decode('utf-8'))
     
     return caption
-    
